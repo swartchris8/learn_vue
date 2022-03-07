635865df24023c47813e1023aebcf8994f5dcf0b
Chris Swart <cswart@outlook.com>
2022-03-07 14:10:53 +0100

    trying to setup vue router

---
+++ todo_and_markdown/src/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Hello World!</h1>
+    <p>
+    <!-- use the router-link component for navigation. -->
+    <!-- specify the link by passing the `to` prop. -->
+    <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
+    <router-link to="/">Go to Home</router-link>
+    <router-link to="/tool">Go to Tool</router-link>
+  </p>
+  <!-- route outlet component matched by the route will render here -->
+  <router-view/>
+</template>
