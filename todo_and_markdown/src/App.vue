3c7d8a1c783f9bcfb0906190ef1e28231fb0479b
Chris Swart <cswart@outlook.com>
2022-03-07 13:43:56 +0100

    first commit of todo list and markdown editor

---
+++ todo_and_markdown/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import MarkdownEditor from './components/MarkdownEditor.vue';
+import TodoList from './components/TodoList.vue';
+
+</script>
+
+<template>
+  
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <p> TEST HEADING </p>
+    
+  </header>
+
+  <main>
+    <TodoList />
+    <MarkdownEditor />
+  </main>
+</template>
